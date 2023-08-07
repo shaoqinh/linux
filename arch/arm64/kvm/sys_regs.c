eb7b15d3ccf50fab4673d32f370c21c02f0db354
Jing Zhang <jingzhangos@google.com>
2023-08-07 09:22:05 -0700

    KVM: arm64: Enable writable for ID_AA64PFR0_EL1
    All valid fields in ID_AA64PFR0_EL1 are writable from usrespace
    with this change.
    
    Signed-off-by: Jing Zhang <jingzhangos@google.com>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -2041,7 +2041,7 @@ static const struct sys_reg_desc sys_reg_descs[] = {
 	  .get_user = get_id_reg,
 	  .set_user = set_id_reg,
 	  .reset = read_sanitised_id_aa64pfr0_el1,
-	  .val = ID_AA64PFR0_EL1_CSV2_MASK | ID_AA64PFR0_EL1_CSV3_MASK, },
+	  .val = GENMASK(63, 0), },
 	ID_SANITISED(ID_AA64PFR1_EL1),
 	ID_UNALLOCATED(4,2),
 	ID_UNALLOCATED(4,3),
